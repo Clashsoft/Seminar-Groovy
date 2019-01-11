7660d2547a5b9ed26bd203ea2bcc559b1677e09c
Adrian Kunz <clashsoft@hotmail.com>
2019-01-11 15:39:35 +0100

    Shorten Zeichenketten and improve code sample

--- src/groovy/string-interpolation.groovy
+++ src/groovy/string-interpolation.groovy
@@ -1,8 +1,7 @@
 #!/usr/bin/env groovy
 def a = 1, b = 2
 assert "$a+$b=${a+b}" == '1+2=3'
-
-def g = "${ -> println 0; a }"
-println g
-a = 3
-println g
+def g = "${ -> a++ }"
+assert g == '1' && g == '2' && g == '3'
+a = 10
+assert g == '10'
