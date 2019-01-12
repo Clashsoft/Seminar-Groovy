6c6a583f4b31648c9be5b2c21d74da37458a88bc
Adrian Kunz <clashsoft@hotmail.com>
2019-01-12 15:49:22 +0100

    Shorten Closures code sample and description

--- src/groovy/closures-dsl.groovy
+++ src/groovy/closures-dsl.groovy
@@ -1,11 +1,8 @@
 #!/usr/bin/env groovy
 def eval(Closure c) {
 	def result = 0
-	c.delegate = new Object() {
-		void add(v) { result += v }
-		void mul(v) { result *= v }
-	}
+	c.delegate = new Object() { void add(v) { result += v };
+	                            void mul(v) { result *= v } }
 	c(); println(result)
 }
-
 eval { add 3; mul 4; add(-1); mul 0.5 }
