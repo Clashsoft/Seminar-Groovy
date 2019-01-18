c3fedae2fe028fcf6f447ace0ba006ce2981ff3b
Adrian Kunz <clashsoft@hotmail.com>
2019-01-18 12:00:03 +0100

    Improve dynamic-typing.groovy

--- src/groovy/dynamic-typing.groovy
+++ src/groovy/dynamic-typing.groovy
@@ -2,6 +2,6 @@
 class Duck { void quack() { println "quack" }}
 
 def duck = new Duck(); duck.quack()
-duck = [quack: { println "I'm not a duck"}] as Duck
-assert duck instanceof Duck; duck.quack()
+duck = [quack: { println "I'm not a duck"}] as Duck; duck.quack()
+assert duck instanceof Duck
 duck = "I'm a string"; duck.quack()
