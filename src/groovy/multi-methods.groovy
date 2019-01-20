d78258a8b68abb92b0df553e6c70acb3a604a34c
Adrian Kunz <clashsoft@hotmail.com>
2019-01-20 14:18:09 +0100

    Improve multi methods code sample and description

--- src/groovy/multi-methods.groovy
+++ src/groovy/multi-methods.groovy
@@ -1,6 +1,5 @@
 #!/usr/bin/env groovy
-String take(Object o) { return "Object" }
-String take(String s) { return "String" }
-
+def take(Object o) { return "Object" }
+def take(String s) { return "String" }
 Object arg = "I'm a String"
 assert take(arg) == "String"
