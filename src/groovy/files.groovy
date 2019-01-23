6dffb5375a7b16d58bd03db7b3aa5e0fee31b775
Adrian Kunz <clashsoft@hotmail.com>
2019-01-23 19:36:04 +0100

    Subsubsection Dateiverarbeitung, with code sample

---
+++ src/groovy/files.groovy
@@ -0,0 +1,5 @@
+def result = new File("all-files.txt"); result.text = ''
+new File('.').eachFileMatch(~/^.*\.groovy$/) {
+	result << "$it.name:\n"
+	it.eachLine { line, num -> result << "$num | $line\n" }
+}
