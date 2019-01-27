cd183e1eee6a399049b485e0733d905ef3687d23
Adrian Kunz <clashsoft@hotmail.com>
2019-01-27 20:55:11 +0100

    Improve Functional Closures example and description

--- src/groovy/closures-fp.groovy
+++ src/groovy/closures-fp.groovy
@@ -1,4 +1,4 @@
 def doubler = { it * 2 }, adder = { a, b -> a + b }
-assert doubler(2) == 4 && adder(1, 2) == 3 && adder('a', 'b') == 'ab'
-assert [3, 1, 5].collect(doubler) == [6, 2, 10] && (1..10).inject(0, adder) == 55
+assert doubler(2) == 4 && adder(1, 2) == 3
+assert [3, 1, 5].collect(doubler) == [6, 2, 10] && [1, 5, 7, 3].inject(0, adder) == 16
 assert (1..7).findAll { it % 2 == 0 } == [2, 4, 6]
