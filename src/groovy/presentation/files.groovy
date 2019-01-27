2d0841a8a172f992b4a3f87cdee83d3449c2a894
Adrian Kunz <clashsoft@hotmail.com>
2019-01-27 20:24:08 +0100

    Remove files.groovy and rename some presentation code files

--- src/groovy/presentation/files.groovy
+++
@@ -1,8 +0,0 @@
-def result = new File("all-files.txt")
-result.text = ''
-new File('.').eachFileMatch(~/^.*\.groovy$/) {
-	result << "${ it.name }:\n"
-	it.eachLine { line, num ->
-		result << "$num | $line\n"
-	}
-}
